870911fcc04ddec0703804de01237274fbc431fb
MESUT PISKIN <mesutpiskin@outlook.com>
2026-05-24 10:06:25 +0300

    feat: implement MessagingAuthenticatorRequiredActionFactory

--- src/main/java/com/mesutpiskin/keycloak/auth/messaging/MessagingAuthenticatorRequiredActionFactory.java
+++ src/main/java/com/mesutpiskin/keycloak/auth/messaging/MessagingAuthenticatorRequiredActionFactory.java
@@ -1,13 +1,18 @@
 package com.mesutpiskin.keycloak.auth.messaging;
-import org.keycloak.authentication.RequiredActionProvider;
+
+import org.keycloak.Config;
 import org.keycloak.authentication.RequiredActionFactory;
+import org.keycloak.authentication.RequiredActionProvider;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
-import org.keycloak.Config;
+
 public class MessagingAuthenticatorRequiredActionFactory implements RequiredActionFactory {
-    @Override public RequiredActionProvider create(KeycloakSession s) { return null; }
+
+    private static final MessagingAuthenticatorRequiredAction SINGLETON = new MessagingAuthenticatorRequiredAction();
+
+    @Override public RequiredActionProvider create(KeycloakSession session) { return SINGLETON; }
     @Override public String getId() { return MessagingAuthenticatorRequiredAction.PROVIDER_ID; }
-    @Override public String getDisplayText() { return ""; }
+    @Override public String getDisplayText() { return "Set up Messaging Authenticator"; }
     @Override public void init(Config.Scope config) {}
     @Override public void postInit(KeycloakSessionFactory factory) {}
     @Override public void close() {}
